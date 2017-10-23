76b410e658b8d85f274446886d01f4dca551b20b
Ruan Aragão <aragao.ruan@gmail.com>
2017-10-23 19:45:27 -0200

    aplicativo, marcacao laboratorio.

---
+++ marcacaoLaboratorio/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MarcacaolaboratorioConfig(AppConfig):
+    name = 'marcacaoLaboratorio'
---
+++ marcacaoLaboratorio/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Laboratorio(models.Model):
+    nome_laboratorio = models.CharField(max_length = 100)
+    sigla_laboratorio = models.CharField(max_length= 3)
+
+    def __unicode__(self):
+        return self.nome_laboratorio
+
+class Marcacao(models.Model):
+    cod_laboratorio = models.ForeignKey(Laboratorio, on_delete=models.CASCADE)
+    cod_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    data_marcacao = models.DateTimeField()
+    hora = models.TimeField()
+    data = models.DateField()
+
+
+    def __unicode__(self):
+        return self.data_marcacao
